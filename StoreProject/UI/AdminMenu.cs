f0b1b365f7b2852de22d050f7b4f96f95c0dd9a4
James Hinson <octobersith@outlook.com>
2023-01-03 01:34:36 -0500

    Menu construction

--- StoreProject/UI/AdminMenu.cs
+++ StoreProject/UI/AdminMenu.cs
@@ -4,13 +4,38 @@ namespace StoreProject
     {
         public void MenuDisplay()
         {
-            Console.WriteLine("Coming Soon");
+            Console.Clear();
+            Console.WriteLine("        ======================================");
+            Console.WriteLine("        =         Logistical Orders          =");
+            Console.WriteLine("        ======================================");
+            Console.WriteLine("         Press 1 to View Orders");
+            Console.WriteLine("         Press 2 to Change Order Status");
+            Console.WriteLine("         Press 3 to Update User Profile");
+            Console.WriteLine("         Press 4 to Add Inventory");
+            Console.WriteLine("         Press 5 to Update Inventory");
+            Console.WriteLine("         Press 0 to Exit");
+            Console.WriteLine("        ======================================");
         }
         public string UserSelection()
         {
-            Console.ReadLine();
-            return "Exit";
+            string userinput = Console.ReadLine();
+            switch (userinput)
+            {
+                case "0":
+                    return "Exit";
+                case "1":
+                    return "ViewOrder";
+                case "2":
+                    return "ChangeOrder";
+                case "3":
+                    return "UpdateProfile";
+                case "4":
+                    return "AddInventory";
+                case "5":
+                    return "UpdateInventory";
+                default:
+                    return "Exit";
+            }
         }
-       
     }
 }
--- StoreProject/UI/CustomerMenu.cs
+++ StoreProject/UI/CustomerMenu.cs
@@ -4,12 +4,35 @@ namespace StoreProject
     {
         public void MenuDisplay()
         {
-            Console.WriteLine("Coming Soon");
+            Console.Clear();
+            Console.WriteLine("        ======================================");
+            Console.WriteLine("        =         Logistical Orders          =");
+            Console.WriteLine("        ======================================");
+            Console.WriteLine("         Press 1 to Create Profile");
+            Console.WriteLine("         Press 2 to Update Profile");
+            Console.WriteLine("         Press 3 to Create an Order");
+            Console.WriteLine("         Press 4 to Check Order Status");
+            Console.WriteLine("         Press 0 to EXIT");
+            Console.WriteLine("        ======================================");
         }
         public string UserSelection()
         {
-            Console.ReadLine();
-            return "Exit";
+            string userinput = Console.ReadLine();
+            switch (userinput)
+            {
+                case "0":
+                    return "Exit";
+                case "1":
+                    return "CreateProfile";
+                case "2":
+                    return "UpdateProfile";
+                case "3":
+                    return "CreateOrder";
+                case "4":
+                    return "TrackOrder";
+                default:
+                    return "Exit";
+            }
         }
        
     }
