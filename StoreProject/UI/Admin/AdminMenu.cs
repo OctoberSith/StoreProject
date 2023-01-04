e16444e6545edd5858eb098f852343586d01f31d
James Hinson <octobersith@outlook.com>
2023-01-04 16:27:41 -0500

    Updates

--- StoreProject/UI/Admin/AdminMenu.cs
+++ StoreProject/UI/Admin/AdminMenu.cs
@@ -9,10 +9,11 @@ namespace StoreProject
             Console.WriteLine("        =         Logistical Orders          =");
             Console.WriteLine("        ======================================");
             Console.WriteLine("         Press 1 to View Orders");
-            Console.WriteLine("         Press 2 to Change Order Status");
+            Console.WriteLine("         Press 2 to Update Order Status");
             Console.WriteLine("         Press 3 to Update User Profile");
-            Console.WriteLine("         Press 4 to Add Inventory");
-            Console.WriteLine("         Press 5 to Update Inventory");
+            Console.WriteLine("         Press 4 to Add Product");
+            Console.WriteLine("         Press 5 to Update Product");
+            Console.WriteLine("         Press 6 to Delete Product");
             Console.WriteLine("         Press 0 to Exit");
             Console.WriteLine("        ======================================");
         }
